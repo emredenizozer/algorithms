e86bf85e9ff4571c2a4cfa7587d349c67d4fe90a
Emre Deniz Özer <e.oezer@reply.de>
2020-06-14 17:35:22 +0200

    Longest common subsequence recursive code was added.

--- longestCommonSubsequence.cpp
+++ longestCommonSubsequence.cpp
@@ -4,6 +4,36 @@
 
 using namespace std;
 
+int longestCommonSubsequenceRecursiveHelper(vector<int> sequence, int pos, int lastNum)
+{
+    if (pos == sequence.size())
+    {
+        return 0;
+    }
+    int firstSum = 0;
+    if (lastNum < sequence[pos])
+    {
+        firstSum = 1 + longestCommonSubsequenceRecursiveHelper(sequence, pos + 1, sequence[pos]);
+    }
+    int secondSum = longestCommonSubsequenceRecursiveHelper(sequence, pos + 1, lastNum);
+    return max(firstSum, secondSum);
+}
+
+int longestCommonSubsequenceRecursive(vector<int> sequence)
+{
+    int seqSize = sequence.size();
+    int maxLen = 0;
+    for (int i = 0; i < seqSize - 1; i++)
+    {
+        int len = longestCommonSubsequenceRecursiveHelper(sequence, i + 1, sequence[i]);
+        if (len > maxLen)
+        {
+            maxLen = len;
+        }
+    }
+    return maxLen + 1;
+}
+
 int longestCommonSubsequence(vector<int> sequence)
 {
     vector<int> totalSum(sequence.size(), 1);
@@ -44,8 +74,9 @@ int longestCommonSubsequence(vector<int> sequence)
 
 int main()
 {
-    vector<int> input = {3, 4, -1, 0, 6, 2, 3};
+    vector<int> input = {3, 4, -1, 0, 6, 2, 3, 4};
     cout << "Maximum subsequence length: " << longestCommonSubsequence(input) << endl;
+    cout << "Maximum subsequence length using recursion: " << longestCommonSubsequenceRecursive(input) << endl;
 
     return 0;
 }
